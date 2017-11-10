5df8fb26b6c7f9abd8b1573ab674fb76c23905ae
Armin Eslami <armin.eslami@gmail.com>
2017-11-09 22:49:50 -0500

    chore: Set up 'styles' tasks in gulpfile
    [ticket: #1]

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,27 @@ const concat = require('gulp-concat');
 const del = require('del');
 
 const zip = require('gulp-zip');
+
+
+//File paths
+const PATH = {
+	STYLES: 'public/scss/style.scss',
+	DIST: 'public/dist',
+}
+
+
+//Styles
+gulp.task('styles', () => {
+	return gulp.src(PATH.STYLES)
+		.pipe(plumber(function(err) {
+			console.log('Error in styles');
+			console.log(err);
+			this.emit('end');
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer({ browsers: ['last 2 versions']}))
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(PATH.DIST))
+		.pipe(livereload());
+});
