93107e9aeaab9862545d5a946207447ed3cf1872
Armin Eslami <armin.eslami@gmail.com>
2017-11-12 00:03:43 -0500

    feat: Set up development web server
    [ticket: #2]

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-app.js
+// Import modules
+const express = require('express');
+const path = require('path');
+
+// Call express
+const app = express();
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Redirect all routes to 'index.html'
+app.get('/*', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Listen for connections on the specified port
+app.listen(3000, () => {
+	console.log('Server listening on port 3000');
+});
